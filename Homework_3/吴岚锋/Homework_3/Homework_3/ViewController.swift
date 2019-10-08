b7eea4dfcc7968f9cdb0b55dafed71c9c3b7ee1c
Apple <apple@AppledeBABA.local>
2019-10-08 14:37:16 +0800

    init homework_3

---
+++ Homework_3/吴岚锋/Homework_3/Homework_3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework_3
+//
+//  Created by Apple on 2019/10/8.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
